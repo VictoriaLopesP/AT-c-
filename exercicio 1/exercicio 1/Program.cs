7a2e67259b558f89c46186f273928eff39ebbfc0
Victória Lopes <130095528+VictoriaLopesP@users.noreply.github.com>
2025-03-28 02:15:59 -0300

    Add files via upload

---
+++ exercicio 1/exercicio 1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Olá, meu nome é Victória!");
+        Console.WriteLine("Nasci em 10/05/1999 e estou aprendendo C#!");
+    }
+}
