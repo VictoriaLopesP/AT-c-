ccf4b77ac28ff212acfe98df89c6ada0c84e4ba8
Victória Lopes <130095528+VictoriaLopesP@users.noreply.github.com>
2025-03-28 02:16:47 -0300

    Add files via upload

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome completo: ");
+        string nome = Console.ReadLine();
+        string nomeCifrado = CifrarNome(nome);
+        Console.WriteLine($"Nome cifrado: {nomeCifrado}");
+    }
+
+    static string CifrarNome(string nome)
+    {
+        char[] caracteres = nome.ToCharArray();
+
+        for (int i = 0; i < caracteres.Length; i++)
+        {
+            char c = caracteres[i];
+
+            if (c >= 'A' && c <= 'Z')
+                caracteres[i] = (char)('A' + (c - 'A' + 2) % 26);
+            else if (c >= 'a' && c <= 'z')
+                caracteres[i] = (char)('a' + (c - 'a' + 2) % 26);
+        }
+
+        return new string(caracteres);
+    }
+}
---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.Write("Digite o número da operação desejada: ");
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+            double resultado = 0;
+            bool operacaoValida = true;
+
+            switch (operacao)
+            {
+                case 1:
+                    resultado = num1 + num2;
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    break;
+                case 4:
+                    if (num2 != 0)
+                        resultado = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                        operacaoValida = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Escolha um número entre 1 e 4.");
+                    operacaoValida = false;
+                    break;
+            }
+
+            if (operacaoValida)
+                Console.WriteLine($"Resultado: {resultado}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Erro: Digite apenas números válidos.");
+        }
+    }
+}
